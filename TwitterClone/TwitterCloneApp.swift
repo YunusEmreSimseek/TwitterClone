1e8c94b86ac08c509fb0d423b6e91e6d8e7dbd7d
Yunus Emre Şimşek <yemresimseek@gmail.com>
2024-08-03 16:00:47 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Emre Simsek on 3.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
